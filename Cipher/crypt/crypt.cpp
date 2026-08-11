160cc957dde28f9a96c276752b0df8082de050b0
JamesDomkus <piratefedora@gmail.com>
2026-08-10 20:46:40 -0700

    Created solution and CPP file
    solution and CPP file only, didn't code anything yes

---
+++ Cipher/crypt/crypt.cpp
@@ -0,0 +1,31 @@
+/* ---------------------------------------------------------------------------
+** I attest that this following code represents my own work and is subject to
+** the plagiarism policy found in the course syllabus.
+**
+** Class: 	CSC 242
+** Assignment: 	Lesson #5
+** File: 	crypt.cpp
+** Description: Ask for user to input a full phone number as a string,
+				then concatenate parts of that string to output that same phone number properly formatted
+**
+** Author: 	James Domkus
+** Date: 	8/10/2026
+** -------------------------------------------------------------------------*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
