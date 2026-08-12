9c35508f01a17f1c3a9d5dec103b17a407283261
JamesDomkus <piratefedora@gmail.com>
2026-08-11 23:09:44 -0700

    Added buildcipher function
    buildCipher function is finished.
    still need encrypt/decrypt functions as well as the main function that reads the command line arguments

--- Cipher/crypt/crypt.cpp
+++ Cipher/crypt/crypt.cpp
@@ -3,7 +3,7 @@
 ** the plagiarism policy found in the course syllabus.
 **
 ** Class: 	CSC 242
-** Assignment: 	Lesson #5
+** Assignment: 	Lesson #5 P8.7
 ** File: 	crypt.cpp
 ** Description: Ask for user to input a full phone number as a string,
 				then concatenate parts of that string to output that same phone number properly formatted
@@ -13,19 +13,63 @@
 ** -------------------------------------------------------------------------*/
 
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+void buildCipher(string key, char cipher[]) {
+	
+	int cipherindex = 0;
+	bool duplicate = false;
+	char backwardAlph[] =
+	{ 'z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a' };
+
+	for (int i = 0;i < key.length();i++) { //step through each letter
+		for (int j = 0;j < i;j++) { //then check if it is the same letter anywhere else in the word
+			if (key[i] == key[j]) {
+				if (i != j) { //make sure we didn't find the same letter
+					//duplicate letter, don't add to cipher
+					duplicate = true; //set duplicate flag 
+				}
+			}
+		}
+
+		if (!duplicate) {
+			//build letter into cipher if not a duplicate letter
+			cipher[cipherindex] = key[i];
+			cipherindex++;
+
+		}
+		else {
+			duplicate = false; //reset duplicate flag before checking the next letter
+		}
+
+	}
+
+	//fill the rest of the array with a backwards alphabet
+	int remainingletters = 26 - cipherindex;
+	for (int k=0;k < remainingletters;k++) {
+		cipher[cipherindex] = backwardAlph[k];
+		cipherindex++;
+	}
+		
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	char cipher[26];
+	char alphabet[] =
+	{ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	string keyword;
+
+	cin >> keyword;//replace with command line arguments
+
+	buildCipher(keyword, cipher);
+
+	//for (int g=0;g < 26;g++) {
+	//	cout << cipher[g];
+	//}
+
+
+}
+
